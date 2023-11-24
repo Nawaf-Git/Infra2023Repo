a369ba15f213a7230ab2c18e83436bcde1739bd0
Nawaf-Git <148770105+Nawaf-Git@users.noreply.github.com>
2023-11-24 22:58:51 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,3 +26,12 @@ resource "alicloud_vswitch" "project_vswitch_2" {
   cidr_block        = "10.0.2.0/24"
   zone_id  = "me-central-1b"
 }
+instance_name        = "project-ecs"
+  image_id             = "centos_7_9_x64_20G_alibase_20231019.vhd"
+  instance_type        = "ecs.g6.large"
+  vswitch_id           = alicloud_vswitch.project_vswitch_1.id
+  internet_max_bandwidth_out = 10
+ instance_charge_type      = "PostPaid"
+ security_groups =[alicloud_security_group.project.id]
+system_disk_category ="cloud_essd"
+}
